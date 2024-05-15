73827cfa10644aaeade422ec230aea32507c8628
THEBIGTOMMY13DEV <141506537+THEBIGTOMMY13DEV@users.noreply.github.com>
2024-05-15 17:33:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let comedyshows=["Young Sheldon" , "The Office","Friends", "The Big Bang Theory" ];
+let actionshows=["Fallout","The Rookie","Masters of the Air","",""];
+let moviesontv=[];
+let animationshows= [];
+let crimeshows = []; 
+let dramashows = [];
+let experimentalshows=[]; 
+let fantasyshows=[]; 
+let historicalshows=[]; 
+let horrorshows= [];
+let romanceshows=[]; 
+let scifishows=[];
+let thriller= []; 
+let western= [];
+let showsthatarebasedongames=[];
+// Make sure to declare your HTML elements as variables! 
+let comedyshowsimage=[];
+let moviesontvimage=[];
+let showsthatarebasedongamesimage=[];
+// Submit Button 
+let submitButton = document.querySelector("button"); 
+submitButton.onclick = function() {
+}; 
+
